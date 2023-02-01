7aa6ca371ef5758817d1cfcdfcc898c7d02ec9ee
Marius Budreika <marius.budreika09@gmail.com>
2023-02-01 14:09:19 +0200

    fethcing and listing countries data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const App = () => {
-  return <div>App</div>;
+interface Country {
+  name: string;
+  region: string;
+  area: number;
+}
+
+const App: React.FC = () => {
+  const [countries, setCountries] = useState<Country[]>([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v2/all?fields=name,region,area")
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {countries.map((country) => (
+          <li key={country.name}>
+            <p>Name: {country.name}</p>
+            <p>Region: {country.region}</p>
+            <p>Area: {country.area}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
